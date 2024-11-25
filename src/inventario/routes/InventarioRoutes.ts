f93d8d2cf0e38ddef30429339e8584f251577c42
Ana-Gutman <ana.gutman2001@gmail.com>
2024-11-25 11:18:54 -0300

    middleware

--- src/inventario/routes/InventarioRoutes.ts
+++ src/inventario/routes/InventarioRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import {getProductState} from "../controllers/InventarioController";
+import authorize from '../../shared/middleware/authMiddleware';
+import accessLogger from '../../shared/middleware/accessLogMiddleware';
 
 const router = Router();
 
-router.get('/inventario/:productId/estado', getProductState);
+router.get('/inventario/:productId/estado', authorize(["Admin", "Supervisor Cocina", "Supervisor Local"]), accessLogger,  getProductState);
 
 export default router;
